84b48ad0be9d37ece762c58effe3e7f6c6569b13
AtitBimali <atitbimali10@gmail.com>
2022-06-20 11:53:15 +0545

    Merge branch 'master' of https://github.com/AtitBimali/marketplace

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     #path("<category>/", views.category, name="category"),
     path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+
 
 ]
